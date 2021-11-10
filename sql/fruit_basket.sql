e286a4906777bcc6cc2a62757161d1aee9ea488e
Moddy Pikiso <moddypikiso17@gmail.com>
2021-11-10 18:38:14 +0200

    fixing travis errors

--- sql/fruit_basket.sql
+++ sql/fruit_basket.sql
@@ -2,6 +2,7 @@ create table fruit_basket (
   id serial not null primary key,
   fruit_name text, 
   quantity number, 
-  price decimal (10,2));
+  price decimal (10,2)
+);
 
   INSERT INTO fruit_basket(fruit_name, quantity, price)VALUES ('Apple', 3, 2);
